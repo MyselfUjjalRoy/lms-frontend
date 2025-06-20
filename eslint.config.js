829c3f4995358637b9d53739ff1dfaafa1f79e4e
Ujjal Roy <royu99099@gmail.com>
2025-06-20 17:37:39 +0530

    configures auto import sorting for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default [
   { ignores: ['dist'] },
@@ -17,22 +18,28 @@ export default [
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      react: { version: '18.3' }
+    },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
     },
   },
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+
+import { Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    lms
+      <Routes>{/* <Route path = "/" element = {<Home />}</Route> */}</Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
